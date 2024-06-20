cb764292a9bc182f92d1e26d8ae5305a3cf333cb
Faluckes <FaluckesDev@protonmail.com>
2024-06-20 16:35:23 -0300

    Adicionar arquivos de projeto.

---
+++ UpdateRagedIPs/GetIPS.cs
@@ -0,0 +1,43 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UpdateRagedIPs
+{
+    public class GetIPS
+    {
+
+        public void getRangeIPS()
+        {
+            var driver = new ChromeDriver();
+            driver.Navigate().GoToUrl("https://www.nslookup.io/domains/www.youtube.com/webservers/");
+
+            //var trCounter = driver.FindElement(By.XPath("\"/html/body/div[1]/div[3]/main/div[3]/table/tbody/tr"));
+            //Console.WriteLine(trCounter);
+            var tabela = driver.FindElement(By.XPath("/html/body/div[1]/div[3]/main/div[3]/table"));
+
+            for (var x = 1; x <= 30; x++)
+            {
+                try
+                {
+                    var ip = driver.FindElement(By.XPath($"/html/body/div[1]/div[3]/main/div[3]/table/tbody/tr[{x}]/td[2]/strong")).Text;
+                    Console.WriteLine(ip);
+
+                }
+                catch(Exception ex) { }
+
+
+            }
+            ////*[@id="app"]/div[3]/main/div[3]/table/tbody/tr[1]
+
+
+
+        }
+
+
+    }
+}
---
+++ UpdateRagedIPs/Program.cs
@@ -0,0 +1,32 @@
+﻿// See https://aka.ms/new-console-template for more information
+using UpdateRagedIPs;
+using static System.Net.Mime.MediaTypeNames;
+
+Console.WriteLine("Program by Faluckes");
+Console.WriteLine("Github: https://github.com/Faluckes \nContact:faluckesdev@protonmail.com\n");
+
+Console.WriteLine("\nSearch IPs\n");
+var Start = new GetIPS();
+var option = 0;
+try
+{
+
+
+    while (!(option == 1 || option == 2))
+    {
+        Console.WriteLine("Select [1] for Start\nSelect [2] for Close Application");
+        option = Convert.ToInt16(Console.ReadLine());
+
+        if (option == 1)
+        {
+            Console.WriteLine("Starting...");
+            Start.getRangeIPS();
+        }
+
+        if (option == 2)
+        {
+            Environment.Exit(0);
+        }
+    }
+}
+catch (Exception err) { Console.WriteLine(err.StackTrace); }
